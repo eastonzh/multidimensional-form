c92bf4c92e54ace4f292c5300ef3de2ee0731257
Denver Zheng <isdhzh@outlook.com>
2023-05-13 02:10:33 +0800

    feat: backend init

---
+++ backend/src/index.ts
@@ -0,0 +1,8 @@
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.listen(20000, () => {
+  // eslint-disable-next-line no-console
+  console.log('feishu-multidimensional-form-backend exec success...');
+});
