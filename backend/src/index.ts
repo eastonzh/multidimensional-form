1948bc08b42c1bd37c8634547c7642d3f3b9f325
Denver Zheng <isdhzh@outlook.com>
2023-05-13 16:14:16 +0800

    chore: update output info format

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,5 +4,5 @@ const app = new Koa();
 
 app.listen(20000, () => {
   // eslint-disable-next-line no-console
-  console.log('feishu-multidimensional-form-backend exec success...');
+  console.log('feishu-multidimensional-form-backend exec success... (port: 20000)');
 });
