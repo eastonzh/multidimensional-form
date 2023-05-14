80df9e4d2b6b5a88ffe4ac0bed5214e667836fde
Denver Zheng <isdhzh@outlook.com>
2023-05-14 14:46:22 +0800

    feat: add koa cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
 
 import routersInstaller from './routers';
 
 const app = new Koa();
 
+app.use(cors());
 app.use(bodyParser());
 routersInstaller(app);
 app.listen(20000, () => {
